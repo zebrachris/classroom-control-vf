73cb564310ab58cbffecdf4aceaaefc62958bd2d
zebrachris <zebrachris@hotmail.com>
2016-10-12 13:15:26 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,7 +1,19 @@
 
 
-class skeleton {
 
+class skeleton { 
+  file { '/etc/skel':
+    ensure => directory, 
+    owner => 'root', 
+    group => 'root', 
+    mode => '0755',
+  }
+  file { '/etc/skel/.bashrc':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
+  } 
 }
-
 
