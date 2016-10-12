77ee1749538eb3511a2442a4ea59fd10e52dc78b
zebrachris <zebrachris@hotmail.com>
2016-10-12 10:20:08 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,15 +42,6 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {
-
-file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Test Me!\n",
-},
-
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
@@ -58,5 +49,11 @@ file { '/etc/motd':
 }
 
 
-
+file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "This worked from GitHub!\n",
+}
 
