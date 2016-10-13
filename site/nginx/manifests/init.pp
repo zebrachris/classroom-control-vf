26272fc8559cbb61ef0687319035c0616c610367
zebrachris <zebrachris@hotmail.com>
2016-10-13 10:47:38 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,18 +3,36 @@ class memcached {
     ensure => present, 
   }
 
-  file { '/etc/nginx/memcached':
+  file { '/etc/nginx/nginx.conf':
     ensure => file,
     owner => 'root',
     group => 'root',
     mode => '0644',
-    source => 'puppet:///modules/nginx/index.html',
-    require => Package['memcached'],
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
   }
-
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  
   service { 'nginx':
     ensure    => running,
     enable    => true,
-    subscribe => File['/etc/nginx/memcached'],
+    subscribe => File['/etc/nginx/nginx.conf', 
+                      '/etc/nginx/conf.d/default.conf',
+                      ''],
   }
 }
