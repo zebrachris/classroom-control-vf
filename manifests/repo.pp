c728dfca98f9792049aa3d800dc234428780255f
zebrachris <zebrachris@hotmail.com>
2016-10-13 11:32:33 -0700

    Create repo.pp

---
+++ manifests/repo.pp
@@ -0,0 +1,8 @@
+yumrepo { 'updates':
+  ensure     => 'present',
+  descr      => 'CentOS-$releasever - Updates',
+  enabled    => '0',
+  gpgcheck   => '1',
+  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+}
