b07d3f47ef3698067c6f3a7f0dba96b0830f45b3
zebrachris <zebrachris@hotmail.com>
2016-10-13 11:32:12 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -54,14 +54,6 @@ node default {
 #  include skeleton
 #}
 
-yumrepo { 'updates':
-  ensure     => 'present',
-  descr      => 'CentOS-$releasever - Updates',
-  enabled    => '0',
-  gpgcheck   => '1',
-  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
-}
 
 #
 #exec { "Run Cowsay":
