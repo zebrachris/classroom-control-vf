de96572a8f4e5c5ab376a1668fb5ccd6c62ae835
zebrachris <zebrachris@hotmail.com>
2016-10-13 12:49:35 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,38 +1,46 @@
-class nginx {
+class nginx { 
   package { 'nginx':
     ensure => present, 
   }
-
-  file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-  }
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
+  file { '/var/www': 
+    ensure => directory, 
+    owner => 'root', g
+    roup => 'root', 
+    mode => '0775',
   }
   file { '/var/www/index.html':
     ensure => file,
     owner => 'root',
     group => 'root',
-    mode => '0644',
+    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/nginx.conf', 
     require => Package['nginx'],
+    notify => Service['nginx'], 
   }
-  
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/nginx.conf', 
-                      '/etc/nginx/conf.d/default.conf',
-                      '/var/www/index.html'],
+  file { '/etc/nginx/conf.d': 
+    ensure => directory, 
+    owner => 'root', 
+    group => 'root', 
+    mode => '0775',
+  }  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root', 
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/default.conf', 
+    require => Package['nginx'],
+    notify => Service['nginx'], 
   }
+  service { 'nginx': 
+    ensure => running, 
+    enable => true,
+  } 
 }
