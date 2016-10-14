57b904c9921c9a591fa3449dfa910b3fc4795f98
zebrachris <zebrachris@hotmail.com>
2016-10-14 09:51:45 -0700

    Update repo.pp

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -14,11 +14,11 @@
 #  source => 'puppet:///modules/nginx/update.repo',
 #}  
   
-yumrepo { 'updates':
-  ensure     => 'present',
-  descr      => 'CentOS-$releasever - Updates',
-  enabled    => '1',
-  gpgcheck   => '1',
-  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
-}
+#yumrepo { 'updates':
+#  ensure     => 'present',
+#  descr      => 'CentOS-$releasever - Updates',
+#  enabled    => '1',
+#  gpgcheck   => '1',
+#  gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+#  mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+#}
