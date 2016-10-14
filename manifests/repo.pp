871e05ced476a310d91fc1de875fe70e36efec10
zebrachris <zebrachris@hotmail.com>
2016-10-14 09:50:51 -0700

    Update repo.pp

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,17 +1,18 @@
 
-file { '/etc/yum.repos.d': 
-  ensure => directory, 
-  owner => 'root',
-  group => 'root',
-  mode => '0775',
-}
-file { '/etc/yum.repos.d/update.repo':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0664',  
-  source => 'puppet:///modules/nginx/update.repo',
-}  
+
+#file { '/etc/yum.repos.d': 
+#  ensure => directory, 
+#  owner => 'root',
+#  group => 'root',
+#  mode => '0775',
+#}
+#file { '/etc/yum.repos.d/update.repo':
+#  ensure => file,
+#  owner => 'root',
+#  group => 'root',
+#  mode => '0664',  
+#  source => 'puppet:///modules/nginx/update.repo',
+#}  
   
 yumrepo { 'updates':
   ensure     => 'present',
