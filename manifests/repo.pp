46f9663c5871b6f7666005948808a4779959278a
zebrachris <zebrachris@hotmail.com>
2016-10-14 09:00:56 -0700

    Update repo.pp

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,7 +1,7 @@
-repo { 'updates':
+yumrepo { 'updates':
   ensure     => 'present',
   descr      => 'CentOS-$releasever - Updates',
-  enabled    => '0',
+  enabled    => '1',
   gpgcheck   => '1',
   gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
   mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
